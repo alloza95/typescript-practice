e5be23b8a8b244a33a046ac4afdc7b83530891dd
Arnau Alloza <a.alloza@zurich.com>
2021-03-29 01:02:58 +0200

    Function return types

--- app.ts
+++ app.ts
@@ -1,29 +1,9 @@
-type Combinable = number | string
-type ConversionDescriptor = "as-number" | "as-text" // This is literal type. Only accepts one of these strings.
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(26, 30, "as-number");
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combinedStringAges = combine("26", "30", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Arnau", "Clara", "as-text");
-console.log(combinedNames);
+printResult(add(1, 4))
