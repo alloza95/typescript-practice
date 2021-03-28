296284f7f64921e979dcaf6a0515288543c52f5a
Arnau Alloza <a.alloza@zurich.com>
2021-03-28 21:55:02 +0200

    Tuple types

--- app.ts
+++ app.ts
@@ -1,19 +1,35 @@
+// Tuple types doesn't exists in Javascript vanilla. Is a TypeScript type.
+// Tuple types allow you to express an array with a 
+// fixed number of elements whose types are known, 
+// but need not be the same. 
+
 // const person: {
 //     name: string,
 //     age: number
 // }
-const person = {
-    name: 'Arnau',
-    age: 25,
-    hobbies: ['Sports', 'Cooking']
-}
+
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Arnau",
+  age: 25,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+//  person.role = [1, 'admin', 'author'] // !! ERROR !! Has three elements when two are expected
+// person.role.push('admin') // CAUTION! You can push. TypeScript doesn't detect this.
+// person.role[1] = 10 // !! ERROR !! You can't assign a number to a string
 
 let favoriteActivities: string[];
-favoriteActivities = ['Sports'];
+favoriteActivities = ["Sports"];
 
-console.log(person.name);
+console.log(person);
 
 for (const hobby of person.hobbies) {
-    console.log(hobby);   
-    // console.log(hobby.map); // !! ERROR !!     
+  console.log(hobby);
+  // console.log(hobby.map); // !! ERROR !!
 }
