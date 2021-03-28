df5883f1e05f5e6e29c49b6e286b44d0a8186216
Arnau Alloza <a.alloza@zurich.com>
2021-03-29 01:18:07 +0200

    Function types and callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (n: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 let combineValues: (a: number, b: number) => number;
 combineValues = add;
 // combineValues = printResult // Error type
@@ -14,3 +19,7 @@ combineValues = add;
 printResult(add(1, 4));
 
 console.log(combineValues(8, 8));
+
+addAndHandle(20, 3, (result) => {
+  console.log(result);
+});
